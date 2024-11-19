0b8a4360079b3e9709485ec5717034a806fe91de
Sagnik-code-coder <sagmuk82@gmail.com>
2024-11-19 11:15:29 +0530

    AI Image Gen

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Imagegen from './Components/Imagegen';
+
+function App() {
+  return (
+    <>
+      <Imagegen/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Imagegen.jsx
@@ -0,0 +1,50 @@
+import React,{useRef, useState} from 'react'
+import './Imagegen.css'
+import image from '../Assests/default_image.svg'
+
+const Imagegen = () => {
+  const [ChngImageUrl,setImageUrl] = useState("/");
+  let inputRef = useRef(null);
+  const ImageGenerator = async () =>
+  {
+    if(inputRef.current.value==="")
+    {
+      return 0;
+    }
+    const response = await fetch (
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "User-Agent":"Chrome"
+        },
+        body: JSON.stringify({
+        prompt: `${inputRef.current.value}`,
+        n: 1,
+        size: "1024x1024",
+        })
+      }
+    );
+    let data = await response.json();
+    //console.log(data);
+    let data_array = data.data;
+    setImageUrl(data_array[0].url);
+  }
+  return (
+    
+    <div className='Head'>
+      <h1>AI Image Generator</h1>
+      <div className='ImageLoading'></div>
+      <div className='Image'><img src={ChngImageUrl==="/"?image:ChngImageUrl}/></div>
+
+      <div className='Searchbbox'>
+        <input type="text" ref={inputRef} placeholder="Enter Image URL" />
+        <button onClick={()=>{ImageGenerator()}}>Generate</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Imagegen;
